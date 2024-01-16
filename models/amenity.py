65286cd56864f7e440ebba02b921eae4256e5a73
zoraaaga <fati.sousse12@gmail.com>
2024-01-16 20:07:49 +0100

    testing

--- models/amenity.py
+++ models/amenity.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
+    """Defines Aminity class"""
     __tablename__ = 'amenities'
 
     name = Column(String(128), nullable=False)
